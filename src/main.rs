628bb062ca73c45c0231154c6e3ddfa51d718ebd
Treadful <mail@treadful.dev>
2026-06-24 06:09:58 +0000

    removed regex as unnecessary

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use regex::Regex;
 use salvo::core::prelude::*;
-use salvo::routing::filters::PathFilter;
 
 #[handler]
 async fn default_handler(_req: &mut Request, res: &mut Response) {
@@ -9,11 +7,9 @@ async fn default_handler(_req: &mut Request, res: &mut Response) {
 
 #[tokio::main]
 async fn main() {
-    PathFilter::register_wisp_regex("foo", Regex::new(r"([a-f0-9]{4})").unwrap());
-
     let acceptor = TcpListener::new("127.0.0.1:8000".to_string()).bind().await;
     let router = Router::new()
-        .push(Router::with_path("/{foo:foo}/{**subpath}").get(default_handler))
+        .push(Router::with_path("/foo/{**subpath}").get(default_handler))
         .push(Router::with_path("/{**subpath}").get(default_handler));
     let service = Service::new(router);
 
